28caf5afac5f7a272621c3d3a640cebaca9b8cac
HHL2021 <80688914+HHL2021@users.noreply.github.com>
2021-03-16 15:40:58 +0800

    Update Product_of_Array_Except_Self .c

--- Product_of_Array_Except_Self .c
+++ Product_of_Array_Except_Self .c
@@ -1,4 +1,9 @@
-/*Product of Array Except Self*/
+/*
+LeetCode: Product of Array Except Self
+*/
+/*
+Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+*/
 #include <stdio.h>
 int num[]={2,3,4,5};
 int product=1;
